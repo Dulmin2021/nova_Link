33e4155c727de95405b35f5c09f4eb6f98572c55
Dulmin2021 <dulmin.edu@gmail.com>
2026-08-18 18:57:16 +0530

    fix(android): restore Material3 imports in HomeScreen.kt

--- android/app/src/main/java/org/novalink/ui/screens/HomeScreen.kt
+++ android/app/src/main/java/org/novalink/ui/screens/HomeScreen.kt
@@ -3,6 +3,7 @@ package org.novalink.ui.screens
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
